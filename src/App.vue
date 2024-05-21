0490d0891d786685379e5e4b8d138488983115b0
shenyj <shenyongjiang@newhope.cn>
2024-05-21 15:29:58 +0800

    fix:15:29

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,38 @@ const handleclick = () => {
 
   window.open(routeUrl.href, "_blank");
 };
+
+const test = () => {
+  console.log(1);
+  Promise.resolve("weq").then((res) => {
+    console.log(res);
+  });
+  let p = new Promise((res, rej) => {
+    console.log(2);
+    setTimeout(() => {
+      console.log(88);
+      res();
+    }, 0);
+  });
+  p.then(() => {
+    console.log(5);
+  });
+  setTimeout(() => {
+    console.log(6);
+  }, 500);
+  setTimeout(() => {
+    console.log(0);
+  }, 0);
+  console.log(10);
+};
+// console.log("test(): ", test());
+const test1 = (params) => {
+  return Promise.resolve(params).then((res) => {
+    console.log(res);
+  });
+};
+let params = true;
+console.log("test1(): ", test1(params));
 </script>
 
 <template>
